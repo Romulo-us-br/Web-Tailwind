c62301693bc223f6ba5b15ec3ded8e2fa8bca8ec
Romulus <romulop.brasil25@gmail.com>
2025-02-26 13:53:28 -0300

    pag invite

--- src/app/invite/page.tsx
+++ src/app/invite/page.tsx
@@ -1,13 +1,47 @@
+import { IconButton } from '@/components/icon-button'
+import { InputField, InputIcon, InputRoot } from '@/components/input'
+import { Copy, Link } from 'lucide-react'
 import Image from 'next/image'
 import logo from '../../assets/logo.svg'
+
 export default function InvitePage() {
   return (
     <div className="min-h-dvh flex items-center justify-between gap-16 flex-col md:flex-row">
       <div className="flex flex-col gap-10 w-full max-w-[550px]">
         <Image src={logo} alt="devstage" width={108.5} height={30} />
         <div className="space-y-2">
-          <h1>Inscrição Confimada!</h1>
-          <p> Para entra no evento, acesse o link enviado para o seu e-mail</p>
+          <h1 className="text-4xl font-semibold font-heading text-gray-100 leading-none">
+            Inscrição Confimada!
+          </h1>
+          <p className="text-gray-300">
+            Para entra no evento, acesse o link enviado para o seu e-mail
+          </p>
+        </div>
+        <div className="space-y-6">
+          <div className="space-y-3">
+            <h2 className="text-gray-200 text-xl font-heading font-semibold leading-none">
+              Indique e ganhe
+            </h2>
+            <p className="text-gray-300">
+              Convide mais pessoas para o evento e concorra a prêmios
+              exclusivos! É só compartilhar o link abaixo e acompanhar as
+              incrições:
+            </p>
+          </div>
+
+          <InputRoot>
+            <InputIcon>
+              <Link className="size-5" />
+            </InputIcon>
+            <InputField
+              readOnly
+              defaultValue="http://localhost:3000/invite/123131"
+            />
+
+            <IconButton>
+              <Copy className="size-5" />
+            </IconButton>
+          </InputRoot>
         </div>
       </div>
     </div>
